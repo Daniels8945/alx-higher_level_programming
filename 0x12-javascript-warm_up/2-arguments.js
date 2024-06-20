8d1a567abc57fa5d15d78cf335a6cbfa30bd40c6
daniels8945 <obaj.daniels@gmail.com>
2024-06-20 12:23:00 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-process.argv = [].length;
-if (process.argv === 0) {
+process.args = process.argv.splice(2);
+
+if (args.length === 0) {
   console.log('No argument');
-} else if (process.argv === 1) {
+} else if (args.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
