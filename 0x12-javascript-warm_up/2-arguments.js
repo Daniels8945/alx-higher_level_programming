6f1fb27aa1ece439fb671ebc86a0c3c44499a28d
daniels8945 <obaj.daniels@gmail.com>
2024-06-20 11:30:32 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const argc = [].length
-if(argc < 1){
-	console.log('No argument');
-}if(argc == 1){
-	console.log('Argument found');
-}else if(argc >= 2){
-	console.log('Arguments found');
+const argc = [].length;
+if (argc < 1) {
+  console.log('No argument');
+} if (argc === 1) {
+  console.log('Argument found');
+} else if (argc >= 2) {
+  console.log('Arguments found');
 }
