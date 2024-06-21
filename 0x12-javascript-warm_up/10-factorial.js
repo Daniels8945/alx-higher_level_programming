27a43c0f6bedbd1b4f355bb73c3e340494d9b3a4
daniels8945 <obaj.daniels@gmail.com>
2024-06-21 10:42:55 -0700

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = Number(process.argv.splice[2]);
+
+function factorial (args) {
+  if (Number.isNaN(args) || args === 1) {
+    return 1;
+  } else {
+    return (args * factorial(args - 1));
+  }
+}
+console.log(factorial(args));
