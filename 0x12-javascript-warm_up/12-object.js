d7d922e56beab989081f5fd3c67907a18fd70a45
daniels8945 <obaj.daniels@gmail.com>
2024-06-21 08:34:45 -0700

    12-object.js

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -2,6 +2,5 @@
 
 const myObject = { type: 'object', value: 12 };
 console.log(myObject);
-const myObject1 = { type: 'object', value: 89 };
-Object.assign(myObject, myObject1);
+myObject.value = 89;
 console.log(myObject);
