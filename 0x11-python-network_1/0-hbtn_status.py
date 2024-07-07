54907a0d49507fe75e7358384f872b7b5d2796d5
daniels8945 <obaj.dsniels@gmail.com>
2024-07-06 18:05:52 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,7 @@
 import urllib.request
 
 
-url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    
+url = urllib.request.Request("https://alx-intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
     body = response.read()
 
