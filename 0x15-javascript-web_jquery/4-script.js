3bc58e83522fa63ba0a717ba3aaa5ae2bcb66a12
daniesl8945 <obaj.daniels@gmail.com>
2024-08-11 03:19:12 +0000

    4-script.js

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+$("#toggle_header").click(function() {
+    var currentClass = $("header").attr("class");
+
+    if (currentClass === "red") {
+      $("header").removeClass("red").addClass("green");
+    } else {
+      $("header").removeClass("green").addClass("red");
+    }
+});
